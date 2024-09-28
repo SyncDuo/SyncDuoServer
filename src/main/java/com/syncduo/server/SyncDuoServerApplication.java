8190d8022f68ae496f0203173beabb15d83d41bb
houance <837507557@qq.com>
2024-09-28 23:00:13 +0800

    初始化工程

---
+++ src/main/java/com/syncduo/server/SyncDuoServerApplication.java
@@ -0,0 +1,13 @@
+package com.syncduo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncDuoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncDuoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syncduo/server/SyncDuoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncduo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncDuoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
