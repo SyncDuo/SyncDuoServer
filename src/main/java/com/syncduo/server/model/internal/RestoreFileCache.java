76618e19c08a37f2f31491a6d5f1374b6aaa2726
houance <837507557@qq.com>
2025-09-11 21:59:58 +0800

    增加 restore file 临时表(by snapshot id 设计), 减少 restore 延迟

---
+++ src/main/java/com/syncduo/server/model/internal/RestoreFileCache.java
@@ -0,0 +1,17 @@
+package com.syncduo.server.model.internal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class RestoreFileCache {
+
+    private String debounceJobKey;
+
+    private String snapshotId;
+
+    private String folderFullPath;
+
+    private String fileFullPath;
+}
