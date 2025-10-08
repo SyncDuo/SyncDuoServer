e390ecea9e0b2b89f03ddae13d919a886ff82ace
houance <837507557@qq.com>
2025-10-09 00:26:10 +0800

    1. 启用 Async 注解

--- src/main/java/com/syncduo/server/SyncDuoServerApplication.java
+++ src/main/java/com/syncduo/server/SyncDuoServerApplication.java
@@ -2,12 +2,14 @@ package com.syncduo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableTransactionManagement
+@EnableAsync
 // todo: 3. 增加 SYNCED FOLDER 但是没有 SYNC FLOW 的提示 4. restic backup, restore, download 需要异步
 public class SyncDuoServerApplication {
 
